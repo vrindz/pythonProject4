9b502bb3c225b8a86616caf4eeccccd2aefcc726
hospital <roshnisat9119@gmail.com>
2023-12-06 21:31:45 +0530

    second commit

--- musicplayer/Musicapp/urls.py
+++ musicplayer/Musicapp/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.home, name='Home'),
+    path('index/', views.index, name="index"),
     path('search/', views.search_songs, name='search_songs'),
+    path('register',views.register,name='register'),
+    path('login_user',views.login_user,name='login_user'),
+    path('logout',views.logout,name='logout'),
+
+
 ]
--- musicplayer/Musicapp/views.py
+++ musicplayer/Musicapp/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.db.models import Q
 from .models import Song
-# Create your views here.
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
+# Create your views here.
+# def base(request):
+#     return render(request, "base.html")
 def index(request):
     allSongs = Song.objects.all().order_by('-last_updated')
     return render(request, template_name="index.html", context={"allSongs": allSongs})
+def home(request):
+    # return HttpResponse("Home Page")
+
+    return render(request, "home.html")
 
 
 def search_songs(request):
@@ -24,3 +35,44 @@ def search_songs(request):
 
     context = {'search_result': search_result, 'search_query': search_query}
     return render(request, template_path, context)
+def logout(request):
+    auth.logout(request)
+    return redirect('Home')
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        cpass = request.POST['cpass']
+        if password == cpass:
+            if User.objects.filter(username=username).exists():
+                messages.add_message(request, messages.ERROR, 'Username already exists')
+                return HttpResponseRedirect(reverse('register'))
+            else:
+                user = User.objects.create_user(username=username,email=email,password=password)
+                user.set_password(password)
+                user.save()
+                return redirect("login_user")
+        else:
+            messages.add_message(request, messages.ERROR, 'password not matching')
+            return HttpResponseRedirect(reverse('register'))
+    else:
+        return render(request,"register.html")
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('index/')
+        else:
+            messages.add_message(request, messages.ERROR, 'Invalid username or password')
+            return HttpResponseRedirect(reverse('login_user'))
+
+
+    else:
+        return render(request, "login.html")
